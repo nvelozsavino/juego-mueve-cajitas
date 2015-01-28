0b7d75a265f53c0d2c483d6ce389184a4298c3d2
Ale <alejandro.veloz@gmail.com>
2015-01-29 00:47:57 +0100

    poniendole otro activity

--- app/src/main/java/com/pocotopocopo/juego/MainActivity.java
+++ app/src/main/java/com/pocotopocopo/juego/MainActivity.java
@@ -87,6 +87,10 @@ public class MainActivity extends Activity {
         Container container = (Container)findViewById(R.id.container);
         TextView textView = (TextView) findViewById(R.id.textView);
         container.setText(textView);
+
+        if (savedInstanceState!=null){
+
+        }
         //    mCamera = getCameraInstance();
 
         // Create our Preview view and set it as the content of our activity.
@@ -96,147 +100,153 @@ public class MainActivity extends Activity {
         //frame = (RelativeLayout) findViewById(R.id.frame);
         //Container container = new Container(getApplicationContext());
         //frame.addView(container);
-  /*      bitmap = decodeSampledBitmapFromResource(getResources(),R.drawable.imagen,maxPiecesW*pieceWidth,maxPiecesH*pieceHeight);
-        Log.d(TAG,"bitmap = "+ bitmap.getWidth()+" , " + bitmap.getHeight());
-        int miniBitmapSizeW=bitmap.getWidth()/maxPiecesW;
-        int miniBitmapSizeH=bitmap.getHeight()/maxPiecesH;
-        Map<Integer,Rect> rectList = new HashMap<>();
-        //Piece leftBorder= new Piece(getApplicationContext(),0,0,(pieceWidth+50)*maxPieces,(pieceHeight+50)*maxPieces,true);
-        //leftBorder.setMovable(false);
-        //pieceList.add(leftBorder);
-        //frame.addView(leftBorder);
-        tol[0]=pieceWidth/2;
-        tol[1]=pieceHeight/2;
-        for (int y=0;y<maxPiecesH;y++) {
-            for (int x = 0; x < maxPiecesW; x++) {
-                //int i = x * maxPiecesH + y;
-                int top, left;
-                left = x * (pieceWidth + paddingPieceX) + paddingLeft + paddingPieceX;
-                top = y * (pieceHeight + paddingPieceY) + paddingTop + paddingPieceY;
-
-                Integer[] pos = new Integer[2];
-                pos[0] = left;//-paddingLeft-paddingPieceX;
-                pos[1] = top;//-paddingTop-paddingPieceY;
-
-                Rect rect = new Rect(x*miniBitmapSizeW,y*miniBitmapSizeH,(x+1)*miniBitmapSizeW,(y+1)*miniBitmapSizeH);
-
-                rectList.put(x+y*maxPiecesH,rect);
-
-                positions.add(pos);
-            }
-        }
-        List<Integer[]> posRand = new ArrayList<>();
-        posRand.addAll(positions);
-        for (int i=0;i<maxPieces;i++){
-            Random rnd = new Random();
-            int posIndex = rnd.nextInt(posRand.size());
-            int left = posRand.get(posIndex)[0];
-            int top = posRand.get(posIndex)[1];
-
-            posRand.remove(posIndex);
-            if (i < maxPieces) {
-
-                Piece piece = new Piece(getApplicationContext(), top, left, pieceWidth, pieceHeight, i + 1);
-                //Log.d(TAG,"no he agregado la imagen de la pieza " + i);
-                //Matrix matrix = new Matrix();
-                //matrix.postScale((float)(pieceWidth/miniBitmapSizeW),(float)(pieceHeight/miniBitmapSizeH));
-                //Log.d(TAG,"llegue aqui");
-                //Bitmap bmp=Bitmap.createBitmap(bitmap,left,top,pieceWidth,pieceHeight,matrix,false);
-
-                Rect rect = rectList.get(i+1);
-                piece.setBitmap(bitmap);
-                piece.setrInic(rect);
-                //Log.d(TAG, "pieza "+ (i+1) + "-" +  rectList.get(posIndex).toString());
-                //Log.d(TAG,"agregue la imagen de la pieza " + i);
-                pieceList.add(piece);
-                piece.setLastPos(i);
-                frame.addView(piece);
-            }
-
-        }
-        int borderTop = 0 + paddingTop;
-        int borderLeft = 0 + paddingLeft;
-        int borderRight= (pieceWidth+paddingPieceX)*maxPiecesW + paddingPieceX + paddingLeft;
-        int borderBottom = (pieceHeight+paddingPieceY)*maxPiecesH + paddingPieceY + paddingTop;
-        for (int i=0;i<4;i++) {
-            int top=0, left=0, width=0, height=0;
-
-            switch (i) {
-                case 0://border left
-                    top = borderTop - paddingTop;
-                    left = borderLeft - paddingLeft;
-                    width = paddingLeft;
-                    height = borderBottom-top;
-                    break;
-                case 1://border top
-                    top = borderTop-paddingTop;
-                    left = borderLeft;
-                    width = borderRight;
-                    height = paddingTop;
-                    break;
-                case 2://border right
-                    top = borderTop;
-                    left = borderRight;
-                    width = paddingLeft;
-                    height = borderBottom;
-                    break;
-                case 3://border bottom
-                    top = borderBottom;
-                    left = borderLeft-paddingLeft;
-                    width = borderRight-left;
-                    height = paddingTop;
-                    break;
-            }
-
-            Piece border = new Piece(getApplicationContext(), top, left, width, height, false);
-            border.setMovable(false);
-            pieceList.add(border);
-            frame.addView(border);
-        }
-
-
-
-
-
-
-
-        frame.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-
-                return touchEvent(event);
-            }
-        });
-
-        physics.addPieces(pieceList);
-
-
-        Piece p1=pieceList.get(0);
-        Piece p2=pieceList.get(1);
-        Piece p3=pieceList.get(2);
-        Piece p4=pieceList.get(3);
-        Piece p5=pieceList.get(4);
-        Piece p6=pieceList.get(5);
-        Piece p7=pieceList.get(6);
-        Piece p8=pieceList.get(7);
-        Piece p9=pieceList.get(8);
-        Piece p10=pieceList.get(9);
-
-       // Log.d(TAG,"Connections 0: \n" + physics);
-        physics.movePiece(p4,Orientation.X,210);
-//        Log.d(TAG,"Connections 1: \n" + physics);
-//        physics.movePiece(p7,Orientation.Y,50);
-//        Log.d(TAG,"Connections 2: \n" + physics);
+//       bitmap = decodeSampledBitmapFromResource(getResources(),R.drawable.imagen,maxPiecesW*pieceWidth,maxPiecesH*pieceHeight);
+//        Log.d(TAG,"bitmap = "+ bitmap.getWidth()+" , " + bitmap.getHeight());
+//        int miniBitmapSizeW=bitmap.getWidth()/maxPiecesW;
+//        int miniBitmapSizeH=bitmap.getHeight()/maxPiecesH;
+//        Map<Integer,Rect> rectList = new HashMap<>();
+//        //Piece leftBorder= new Piece(getApplicationContext(),0,0,(pieceWidth+50)*maxPieces,(pieceHeight+50)*maxPieces,true);
+//        //leftBorder.setMovable(false);
+//        //pieceList.add(leftBorder);
+//        //frame.addView(leftBorder);
+//        tol[0]=pieceWidth/2;
+//        tol[1]=pieceHeight/2;
+//        for (int y=0;y<maxPiecesH;y++) {
+//            for (int x = 0; x < maxPiecesW; x++) {
+//                //int i = x * maxPiecesH + y;
+//                int top, left;
+//                left = x * (pieceWidth + paddingPieceX) + paddingLeft + paddingPieceX;
+//                top = y * (pieceHeight + paddingPieceY) + paddingTop + paddingPieceY;
 //
-//        physics.movePiece(p3,Orientation.X,110);
-//        Log.d(TAG,"Connections 3: \n" + physics);
+//                Integer[] pos = new Integer[2];
+//                pos[0] = left;//-paddingLeft-paddingPieceX;
+//                pos[1] = top;//-paddingTop-paddingPieceY;
 //
-//        physics.movePiece(p3,Orientation.Y,20);
-//        Log.d(TAG,"Connections 4: \n" + physics);
+//                Rect rect = new Rect(x*miniBitmapSizeW,y*miniBitmapSizeH,(x+1)*miniBitmapSizeW,(y+1)*miniBitmapSizeH);
 //
-*/
+//                rectList.put(x+y*maxPiecesH,rect);
+//
+//                positions.add(pos);
+//            }
+//        }
+//        List<Integer[]> posRand = new ArrayList<>();
+//        posRand.addAll(positions);
+//        for (int i=0;i<maxPieces;i++){
+//            Random rnd = new Random();
+//            int posIndex = rnd.nextInt(posRand.size());
+//            int left = posRand.get(posIndex)[0];
+//            int top = posRand.get(posIndex)[1];
+//
+//            posRand.remove(posIndex);
+//            if (i < maxPieces) {
+//
+//                Piece piece = new Piece(getApplicationContext(), top, left, pieceWidth, pieceHeight, i + 1);
+//                //Log.d(TAG,"no he agregado la imagen de la pieza " + i);
+//                //Matrix matrix = new Matrix();
+//                //matrix.postScale((float)(pieceWidth/miniBitmapSizeW),(float)(pieceHeight/miniBitmapSizeH));
+//                //Log.d(TAG,"llegue aqui");
+//                //Bitmap bmp=Bitmap.createBitmap(bitmap,left,top,pieceWidth,pieceHeight,matrix,false);
+//
+//                Rect rect = rectList.get(i+1);
+//                piece.setBitmap(bitmap);
+//                piece.setrInic(rect);
+//                //Log.d(TAG, "pieza "+ (i+1) + "-" +  rectList.get(posIndex).toString());
+//                //Log.d(TAG,"agregue la imagen de la pieza " + i);
+//                pieceList.add(piece);
+//                piece.setLastPos(i);
+//                frame.addView(piece);
+//            }
+//
+//        }
+//        int borderTop = 0 + paddingTop;
+//        int borderLeft = 0 + paddingLeft;
+//        int borderRight= (pieceWidth+paddingPieceX)*maxPiecesW + paddingPieceX + paddingLeft;
+//        int borderBottom = (pieceHeight+paddingPieceY)*maxPiecesH + paddingPieceY + paddingTop;
+//        for (int i=0;i<4;i++) {
+//            int top=0, left=0, width=0, height=0;
+//
+//            switch (i) {
+//                case 0://border left
+//                    top = borderTop - paddingTop;
+//                    left = borderLeft - paddingLeft;
+//                    width = paddingLeft;
+//                    height = borderBottom-top;
+//                    break;
+//                case 1://border top
+//                    top = borderTop-paddingTop;
+//                    left = borderLeft;
+//                    width = borderRight;
+//                    height = paddingTop;
+//                    break;
+//                case 2://border right
+//                    top = borderTop;
+//                    left = borderRight;
+//                    width = paddingLeft;
+//                    height = borderBottom;
+//                    break;
+//                case 3://border bottom
+//                    top = borderBottom;
+//                    left = borderLeft-paddingLeft;
+//                    width = borderRight-left;
+//                    height = paddingTop;
+//                    break;
+//            }
+//
+//            Piece border = new Piece(getApplicationContext(), top, left, width, height, false);
+//            border.setMovable(false);
+//            pieceList.add(border);
+//            frame.addView(border);
+//        }
+//
+//
+//
+//
+//
+//
+//
+//        frame.setOnTouchListener(new View.OnTouchListener() {
+//            @Override
+//            public boolean onTouch(View v, MotionEvent event) {
+//
+//                return touchEvent(event);
+//            }
+//        });
+//
+//        physics.addPieces(pieceList);
+//
+//
+//        Piece p1=pieceList.get(0);
+//        Piece p2=pieceList.get(1);
+//        Piece p3=pieceList.get(2);
+//        Piece p4=pieceList.get(3);
+//        Piece p5=pieceList.get(4);
+//        Piece p6=pieceList.get(5);
+//        Piece p7=pieceList.get(6);
+//        Piece p8=pieceList.get(7);
+//        Piece p9=pieceList.get(8);
+//        Piece p10=pieceList.get(9);
+//
+//       // Log.d(TAG,"Connections 0: \n" + physics);
+//        physics.movePiece(p4,Orientation.X,210);
+////        Log.d(TAG,"Connections 1: \n" + physics);
+////        physics.movePiece(p7,Orientation.Y,50);
+////        Log.d(TAG,"Connections 2: \n" + physics);
+////
+////        physics.movePiece(p3,Orientation.X,110);
+////        Log.d(TAG,"Connections 3: \n" + physics);
+////
+////        physics.movePiece(p3,Orientation.Y,20);
+////        Log.d(TAG,"Connections 4: \n" + physics);
+////
+
     }
 
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+
+        super.onSaveInstanceState(outState);
+
+    }
 
     /** A basic Camera preview class */
     public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback {
