cff28d043ab580cf8597a408863f53cb3fdf0269
nico <nicolas.veloz.savino@gmail.com>
2015-04-05 22:50:01 -0500

    Multiplayer working, check TODOs to see what's missing

---
+++ app/src/main/java/com/pocotopocopo/juego/activities/GameListActivity.java
@@ -0,0 +1,15 @@
+package com.pocotopocopo.juego.activities;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+
+/**
+ * Created by nico on 5/04/15.
+ */
+public class GameListActivity extends ListActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
